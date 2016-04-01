a6bf7e7418583807456bd649157a18b8cfa89346
noahCu <NoahSu.CU@gmail.com>
2016-04-01 18:00:58 +0800

    fix input

--- test.cpp
+++ test.cpp
@@ -5,6 +5,21 @@
 #include"lib.h"
 #include<string>
 
+
+int display( char filename[] ) {
+	std::cout<< "display content of " << filename << std::endl;
+	std::cout<< "---------------\n";
+
+	std::ifstream file;
+	file.open( filename );
+	while (1) {
+		char ch = file.get();
+		if (ch == EOF) break;
+		std::cout<< ch;
+	}
+	std::cout<< "---------------\n";
+}
+
 int printSCC(const std::vector<Map> & SCC, const BigMap & SCCMap) {
 	for (auto itr = SCC.begin(); itr != SCC.end(); itr++) {
 		for (auto itrr = itr->v.begin(); itrr != itr->v.end(); itrr++ ) {
@@ -29,7 +44,7 @@ void make_data( int N, int M, int K) {
 	topo.close();
 	
 	std::ofstream demand;
-	demand.open( "deman.csv" );
+	demand.open( "demand.csv" );
 	std::vector<bool> used(N, 0);
 	demand << rand() % N << ',' << rand() % N << ',';
 	while ( K-- ) {
@@ -43,15 +58,36 @@ void make_data( int N, int M, int K) {
 }
 
 
+void testcriPath(Map & omap) {
+	YenPath res;
+	Map reEdge;
+	omap.reverse(reEdge);
+	omap.criPath( omap.s[0], omap.t[0], reEdge, res);
+	if ( res.edge.empty() ) std::cout<< "no possible res\n";
+	for (auto itr = res.edge.begin(); itr != res.edge.end(); itr++) std::cout<< *itr << " -> ";
+}
+	
+
 int mainTest() {
 	Map omap;
 	std::map< int, bool> isCritical;
-	make_data( 600, 600 * 8 , 50);
+
+
+	//int N = 600, M = 600 * 8, K = 0;
+	//std::cin >> N >> M >> K;
+	//make_data( N, M, K);
+
+
 	inputMap(std::string("topo.csv"), std::string("demand.csv"), omap, isCritical);
 
+	/*
 	std::vector<Map> SCC;
 	BigMap SCCMap;
 	setSCC ( omap, SCC, SCCMap);
 	printSCC( SCC, SCCMap );
+	*/
+
+	//testcriPath(omap);
+
 	return 0;
 }
